32b2f67f96bcf8fde7ec00dee6b99242d7c9da98
luigisaetta <luigi.saetta@gmail.com>
2024-04-29 16:59:55 +0200

    added template for config_private

---
+++ template_4_config_private.py
@@ -0,0 +1,13 @@
+"""
+Author: Luigi Saetta
+Date created: 2024-04-27
+Date last modified: 2024-04-28
+Python Version: 3.11
+"""
+
+# OCI
+COMPARTMENT_ID = "ocid1.compartment.oc1..xxyyzz"
+
+# API Keys
+LANGSMITH_API_KEY = "your_api_key"
+COHERE_API_KEY = "your_api_key"
